475a8ace9ba8e278c2a15c3dde675fec2e2d16db
ito <yorizo.ito@gmail.com>
2020-09-25 23:27:40 +0000

    heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,81 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ja">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <meta charset="UTF-8">
+        <title>Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="旅行に特化したSNSです。画像やストーリーを投稿したり、質問することもできます！">
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     </head>
+    
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <header>
+            <div class="navbar">
+                <ul>
+                    <li><a href="#profile"><img src="https://img.icons8.com/windows/32/000000/gender-neutral-user.png"/>Profile</a></li>
+                    <li class="ml-3 ml-md-5"><a href="#skill"><img src="https://img.icons8.com/windows/32/000000/find-matching-job.png"/>Skill</a></li>
+                    <li class="ml-3 ml-md-5 mr-3 mr-md-5"><a href="#work"><img src="https://img.icons8.com/windows/32/000000/internet-browser.png"/>Work</a></li>
+                </ul>
+            </div>
+        </header>
+        
+        <main>
+            <div class="content col-12">
+                <div class="header-image">
+                    <p>yuki's portfolio</p>
                 </div>
             </div>
-        </div>
+            
+            <div class="profile container">
+                <div class="title"><img src="https://img.icons8.com/windows/32/000000/gender-neutral-user.png"><a name="profile">Profile</a></div>
+                <div class="row profile-list col-10 offset-1">
+                    <p>はじめまして。平成5年生まれの26歳、伊藤ゆうきと申します。</p>
+                    <p>
+                        大学では植物に関する研究をしておりました。卒業後は、ホテル業界でフロントを1年2か月経験し、
+                        その後、Webメディアのスタートアップ企業で営業事務や編集部を1年6か月経験しました。
+                    </p>
+                    <p>退職後はプログラミングスクールに通い、現在はエンジニアを目指し自分でサイトを作成するなどしております。</p>
+                    <p>趣味：旅行、バスケ観戦、植物育成、ランニング</p>
+                    <p>ボランティア活動：約1年間カンボジアで日本語を教える活動、佐渡島のトキ保全活動、地元の森林保護活動など</p>
+                </div>
+            </div>
+            
+            <div class="skill container">
+                <div class="title"><img src="https://img.icons8.com/windows/32/000000/find-matching-job.png"/><a name="skill">Skill</a></div>
+                <div class="row profile-list col-10 offset-1">
+                    <ul>
+                        <li>言語・・・HTML, CSS, PHP, JavaScript</li>
+                        <li>フレームワーク、ライブラリ・・・Laravel, Vue.js</li>
+                        <li>ツール・・・Git</li>
+                        <li>OS・・・Windows</li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="work container pb-5">
+                <div class="title col-10"><img src="https://img.icons8.com/windows/32/000000/internet-browser.png"><a name="work">Work</a></div>
+                <div class="row profile-list col-10 offset-1">
+                    <img src="travenirs.png" width="70%" height="auto" class="offset-2">
+                    <p class="col-12 mt-4">●サイト名：Travenirs</p>
+                    <p class="col-12">●コンセプト：『リアルな』旅を共有しよう！</p>
+                    <p class="col-12">●サイト名の由来：Travel(旅行) × Souvenir(お土産)の造語</p>
+                    <p class="col-12">
+                        ●目的：あふれた情報の中からピンポイントで見つけられるように、旅行に特化したSNSを作成しました。
+                        ガイドブックには載っていないような場所やレストランなど、リアルな体験を共有したり知ることができます。
+                        また、初めての旅行や危ないと言われている地域についてなど、実際に行った人に質問することでより充実した旅になります。
+                    </p>
+                    <p class="col-12">●URL：<a href="https://github.com/itonick/travenirs" target="_blank" rel="noopener noreferrer">https://github.com/itonick/travenirs</a></p>
+                    <p class="col-12">●GitHub：<a href="https://travenirs.herokuapp.com/" target="_blank" rel="noopener noreferrer">https://travenirs.herokuapp.com/</a></p>
+                </div>
+            </div>
+            <div class="pt-2 pb-2 text-center bg-dark">
+                <div class="col-md-10">
+                    <p class="mb-0" style="color:white;">&copy; 2020 Yuki Ito</p>
+                </div>
+            </div>
+        </main>
+        
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
     </body>
 </html>
